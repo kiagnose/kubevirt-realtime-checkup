fcdc9f00a963273ae2e2aad25f7ff7801e7e0281
Orel Misan <omisan@redhat.com>
2024-02-19 18:16:09 +0200

    Checkup, Setup: Create VM under test ConfigMap
    In order to enable the checkup the ability to create a ConfigMap
    containing a boot script to be attached to the
    VM undet test, add creation of an empty ConfigMap object to the
    setup flow.
    
    The above ConfigMap is owned by the checkup, so it
    will be garbage collected by K8s, in case of a failure.
    
    Signed-off-by: Orel Misan <omisan@redhat.com>

---
+++ pkg/internal/checkup/configmap/configmap_test.go
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the kiagnose project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2023 Red Hat, Inc.
+ *
+ */
+
+package configmap_test
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+
+	k8scorev1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/kiagnose/kubevirt-realtime-checkup/pkg/internal/checkup/configmap"
+)
+
+func TestNew(t *testing.T) {
+	name := "my-cm"
+	ownerName := "my-pod"
+	ownerUID := "1234567890"
+
+	actualConfigMap := configmap.New(name, ownerName, ownerUID)
+
+	expectedConfigMap := &k8scorev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion: "v1",
+					Kind:       "Pod",
+					Name:       ownerName,
+					UID:        types.UID(ownerUID),
+				},
+			},
+		},
+		Data: nil,
+	}
+
+	assert.Equal(t, expectedConfigMap, actualConfigMap)
+}
